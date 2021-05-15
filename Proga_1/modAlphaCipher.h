e911b5ea4be8ee862b0f46838c297b74464831b4
kali <daniel.sockol0v@yandex.ru>
2021-05-15 13:16:55 +0300

    v1

---
+++ Proga_1/modAlphaCipher.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Exception.h"
+#include <vector>
+#include <string>
+#include <map>
+#include <locale>
+using namespace std;
+class modAlphaCipher
+{
+private:
+    wstring numAlpha =L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; //алфавит по порядку
+    map <char,int> alphaNum; //ассоциативный массив "номер по символу"
+    vector <int> key; //ключ
+    vector<int> convert(const wstring& s); //преобразование строка-вектор
+    wstring convert(const vector<int>& v); //преобразование вектор-строка
+    wstring getValidKey(const wstring & s);
+    wstring getValid_Str(const wstring & s);
+public:
+    modAlphaCipher()=delete; //запретим конструктор без параметров
+    modAlphaCipher(const wstring& skey); //конструктор для установки ключа
+    wstring encrypt(const wstring& open_text); //зашифрование
+    wstring decrypt(const wstring& cipher_text);//расшифрование
+};
